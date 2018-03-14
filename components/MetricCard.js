6c85b5a4737e135d6a078473c935b90520c5b066
Justin Liang <justin.liang@avigilon.com>
2018-03-13 18:15:26 -0700

    create react-native navigation in the app

--- components/MetricCard.js
+++ components/MetricCard.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import DateHeader from './DateHeader';
-import { getMetricMetaInfo } from '../utils/helpers';
-import { grey } from '../utils/colors'
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import DateHeader from './DateHeader'
+import { getMetricMetaInfo } from '../utils/helpers'
+import { gray } from '../utils/colors'
 
 export default function MetricCard ({ date, metrics }) {
   return (
     <View>
-      {data && <DateHeader date={date} />}
+      {date && <DateHeader date={date} />}
       {Object.keys(metrics).map((metric) => {
         const { getIcon, displayName, unit, backgroundColor } = getMetricMetaInfo(metric)
-
         return (
           <View style={styles.metric} key={metric}>
             {getIcon()}
@@ -18,20 +17,20 @@ export default function MetricCard ({ date, metrics }) {
               <Text style={{fontSize: 20}}>
                 {displayName}
               </Text>
+              <Text style={{fontSize: 16, color: gray}}>
+                {metrics[metric]} {unit}
+              </Text>
             </View>
-            <Text style={{fontSize: 16, color: grey}}>
-              {metrics[metric]} {unit}
-            </Text>
           </View>
         )
-      }) }
+      })}
     </View>
   )
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   metric: {
     flexDirection: 'row',
-    marginTop: 12,
-  }
+    marginTop: 12
+  },
 })
